7d609cc2b331f1b1d2dce0906ce86a28d28d6c21
spaghettinoodles <49127161+spaghettinoodles@users.noreply.github.com>
2021-09-29 17:09:11 -0500

    fixing branch

--- Client/client.py
+++ Client/client.py
@@ -1,32 +1,10 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QSizeGrip
-from Client.control_panel import Ui_MainWindow
+from Client.control_panel import control_panel
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    flags = QtCore.Qt.FramelessWindowHint
-    MainWindow.setWindowFlag(flags)
-    MainWindow.show()
+    gui = control_panel()
+    gui.show()
     sys.exit(app.exec_())
-
-
-def mousePressEvent(self, e):
-    self.previous_pos = e.globalPos()
-
-
-def mouseMoveEvent(self, e):
-    delta = e.globalPos() - self.previous_pos
-    self.move(self.x() + delta.x(), self.y()+delta.y())
-    self.previous_pos = e.globalPos()
-
-    self._drag_active = True
-
-
-def mouseReleaseEvent(self, e):
-    if self._drag_active:
-        self.save()
-        self._drag_active = False
