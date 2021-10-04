cb1c9abfe8596677191866f36468629d3e36b343
spaghettinoodles <rkaczynski@uiowa.edu>
2021-10-03 19:35:14 -0500

    laptop push

--- Client/client.py
+++ Client/client.py
@@ -1,6 +1,5 @@
 import sys
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QSizeGrip
+from PyQt5 import QtWidgets
 from Client.control_panel import control_panel
 
 if __name__ == "__main__":
