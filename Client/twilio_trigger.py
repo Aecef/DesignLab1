fc460bcdd6c6854ec2210e2a9410f0e17eaa5d7c
Alec Cleofe <alec-cleofe@uiowa.edu>
2021-10-07 10:59:39 -0500

    added original twilio code to send a text

--- Client/twilio_trigger.py
+++ Client/twilio_trigger.py
@@ -1,8 +1,23 @@
+from twilio.rest import Client
 
 upper_limit = 33.0 #Upper Limit trigger for twilio in Celsius
 lower_limit = 13.0 #Lower Limit trigger for twilio in Celsius
 
 temp_moved_limit = False
+
+account_sid = 'ACa62404846b80019bfc75dc5c3503db23'
+auth_token = 'b2e808da58fc8417a884c56d565f7210'
+
+twil_phone = '+19142923936'
+
+client = Client(account_sid, auth_token)
+
+def sendAlert(alert_message, user_phone):
+    message = client.messages.create(
+        body = alert_message,
+        from_= twil_phone,
+        to = ("+" + user_phone)
+    )
 
 def set_temp_moved_limit(val):
     global temp_moved_limit
